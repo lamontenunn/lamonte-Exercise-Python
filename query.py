aa237354226f2ea45e5eca5d85fda3bcd7f32662
LaMonte <lnunn6@emich.edu>
2023-10-30 18:51:55 -0400

    feat: added menu option
    I added a menu option in the query file.
    When the file is ran it will ask the user to enter a choice
    betweewn 1,2,3 and q with options
    
    Issue: Task1

--- query.py
+++ query.py
@@ -1,3 +1,23 @@
-from movies import Movies
+def menu():
+    while True:
+        print("Options:")
+        print("1: List all movies")
+        print("2: Search movies by names")
+        print("3: Search movies by cast")
+        print("q: Quit")
+        
+        choice = input("\nEnter your choice: ")
+        
+        if choice == 'q' or choice == 'Q':
+            break
+        elif choice == '1':
+            pass  # Code for listing all movies 
+        elif choice == '2':
+            pass  # Code for searching movies by names 
+        elif choice == '3':
+            pass  # Code for searching movies by cast
+        else:
+            print("Invalid choice. Try again.")
 
-movies = Movies('./movies.txt')
+
+menu()
